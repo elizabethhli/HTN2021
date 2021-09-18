b029625746c4f1d09b3a401e64e8881cb8c8589f
Dioceus <chuwei.chen2001@gmail.com>
2021-09-18 09:33:52 -0400

    setup firebase

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+gi
+app.listen(5001, () =>{
+    console.log("Listening on port 5001");
+})
