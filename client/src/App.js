4fdafb83263cc871b096a09e81ef5fdc136dc2f4
Nivetha Siva <ra.n.dom041802@gmail.com>
2021-09-19 00:06:08 -0400

    fixed merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
 import React, {Component} from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Signup from './Signup';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login';
+import Profile from './Profile';
 
 import ProfileCard from './components/ProfileCard/ProfileCard';
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <BrowserRouter>
         <Switch>
-          <Route path = "/dashboard" component = {Dashboard} />
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
         </Switch>
-      </Router>
-  );
-}
+      </BrowserRouter>
+    </div>
+
+)}
 
 export default App;
