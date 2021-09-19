cce3aeb0c8b197903c0d5968ddd98679cd750d29
Dioceus <chuwei.chen2001@gmail.com>
2021-09-19 01:06:02 -0400

    finish auth UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, {Component} from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Signup from './Signup';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Dashboard from './Dashboard'
 import Login from './Login';
 import Profile from './Profile';
 
-import ProfileCard from './components/ProfileCard/ProfileCard';
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
@@ -15,18 +12,10 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
+          <Route exact path="/" component = {Signup} exact/>
+          <Route path="/login" component = {Login} /> 
+          <Route path="/dashboard" component = {Dashboard} />
+          <Route path="/profile" component = {Profile} />
         </Switch>
       </BrowserRouter>
     </div>
